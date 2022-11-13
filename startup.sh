9461ae9d6ae458f701a2e941916ee7d7d2dbf876
dominikn <dominik.nwk@gmail.com>
2022-11-13 15:48:23 +0100

    allow no password

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@
 export VNC_SERVER_PORT=5901
 export NOVNC_PORT=8081
 
+envsubst < /nginx.conf.template > /etc/nginx/conf.d/default.conf
+
 if [[ -z "${DISPLAY}" ]]; then
     echo "ERROR | You need to run a display server first"
     exit 1
@@ -10,15 +12,13 @@ fi
 
 if [[ -z "${PASSWORD}" ]]; then
     echo "no password set"
+    nohup x11vnc -rfbport ${VNC_SERVER_PORT} -display ${DISPLAY} -loop > /dev/null 2>&1 &
 else
     mkdir /root/.vnc
     x11vnc -storepasswd ${PASSWORD} /root/.vnc/passwd
+    nohup x11vnc -rfbauth /root/.vnc/passwd -rfbport ${VNC_SERVER_PORT} -display ${DISPLAY} -loop > /dev/null 2>&1 &
 fi
 
-envsubst < /nginx.conf.template > /etc/nginx/conf.d/default.conf
-# cat /etc/nginx/conf.d/default.conf
-
-nohup x11vnc -rfbauth /root/.vnc/passwd -rfbport ${VNC_SERVER_PORT} -display ${DISPLAY} -loop > /dev/null 2>&1 &
 /usr/share/novnc/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_SERVER_PORT} > /dev/null 2>&1  &
 
 echo ""
